4d2a441de0007d492e7301587c2b3f3746ccc5e2
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-04-07 20:09:28 +0400

    starting express server using express

--- express-course/src/index.js
+++ express-course/src/index.js
@@ -1,12 +1,5 @@
-const http = require("http");
+const app = require("./server");
 
-const server = http.createServer(async (req, res) => {
-  if (req.method === "GET" && req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end();
-  }
-});
-
-server.listen(3001, () => {
+app.listen(3001, () => {
   console.log(`server on http://localhost:3001`);
 });
