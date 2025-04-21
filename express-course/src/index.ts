7cf8a2a7eac48a26a773c3d139f158bcd08459f6
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-04-21 19:41:28 +0400

    implement configurations

--- express-course/src/index.ts
+++ express-course/src/index.ts
@@ -1,8 +1,9 @@
 import app from "./server";
 import * as dotenv from "dotenv";
+import config from "./config";
 
 dotenv.config();
 
-app.listen(3001, () => {
-  console.log(`server on http://localhost:3001`);
+app.listen(config.port, () => {
+  console.log(`server on http://localhost:${config.port}`);
 });
